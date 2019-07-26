082e24e06e9201ab2df4e4a34e5461794551e554
Resi Respati <resir014@gmail.com>
2019-07-27 00:45:49 +0700

    feat: next 9 support (#4)
    * feat: next 9 support
    * remove `App` test
    
    BREAKING CHANGE: This library now requires Next.js 9 or above.

--- test/withAnalytics.test.tsx
+++ test/withAnalytics.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import * as React from 'react';
-import { NextFC } from 'next';
+import { NextPage } from 'next';
 import { render, cleanup } from '@testing-library/react';
 
 import Router from 'next/router';
@@ -9,14 +9,19 @@ import { withAnalytics } from '../src/index';
 
 import 'jest-dom/extend-expect';
 
-jest.mock('next/router');
+jest.mock('next/router', () => ({
+  events: {
+    on: jest.fn(f => f),
+    off: jest.fn(f => f),
+  },
+}));
 jest.mock('react-ga');
 
 afterEach(cleanup);
 
 describe('withAnalytics', () => {
   test('renders correctly', () => {
-    const TestPage: NextFC = () => <div>Example page</div>;
+    const TestPage: NextPage = () => <div>Example page</div>;
     const WithAnalytics = withAnalytics(Router)(TestPage);
     const { container } = render(<WithAnalytics />);
 

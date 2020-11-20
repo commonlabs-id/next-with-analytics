4379a16726bb991a8a328770d7c16343754db0a8
Resi Respati <resir014@gmail.com>
2020-11-20 17:03:40 +0700

    deps upgrade (2020-11-20) (#56)

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,18 +25,34 @@ module.exports = {
   },
   rules: {
     'dot-notation': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
     'no-undef': 'off',
     'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'jsx-a11y/anchor-is-valid': 'off',
     'react/jsx-closing-tag-location': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.tsx'] }],
     'react/jsx-one-expression-per-line': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: true, classes: true, variables: true },
     ],
   },
   settings: {
@@ -44,18 +60,11 @@ module.exports = {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
+      typescript: {},
     },
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
   },
   overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-      },
-    },
     {
       files: ['.eslintrc.js', '*.config.js'],
       parserOptions: { sourceType: 'script' },
--- src/helpers/debug.ts
+++ src/helpers/debug.ts
@@ -1,25 +1,25 @@
 /* eslint-disable no-console */
 
-export function init(trackingCode?: string) {
+export function init(trackingCode?: string): void {
   console.log(`[Analytics] Init triggered for ${trackingCode}`);
 }
 
-export function pageview() {
+export function pageview(): void {
   if (typeof window !== 'undefined') {
     console.log(`[Analytics] Pageview triggered for ${window.location.pathname}`);
   }
 }
 
-export function event(category = '', action = '') {
+export function event(category = '', action = ''): void {
   console.log(`[Analytics] Event for category ${category} and action ${action} triggered`);
 }
 
-export function exception(description = '', fatal = false) {
+export function exception(description = '', fatal = false): void {
   console.log(
     `[Analytics] ${fatal ? 'Fatal exception' : 'Exception'} with description ${description}`,
   );
 }
 
-export function ga(...args: any[]) {
+export function ga(...args: any[]): void {
   console.log(`[Analytics] Called 'ga' function with args ${JSON.stringify(args)}`);
 }
